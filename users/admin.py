3e9ece49ea479d782d4fc499cf3a60875e234a20
Luteranya Nyange <dieume.dev@gmail.com>
2020-08-15 17:25:18 +0300

    customizing the admin dashboard

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
+    """ Custom User Admin """
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'Custom Profile',
+            {
+                'fields': (
+                    'avatar',
+                    'gender',
+                    'date_of_birth',
+                    'bio',
+                    'language',
+                    'currency',
+                    'is_superhost'
+                )
+            }
+        ),
+    )
     pass
